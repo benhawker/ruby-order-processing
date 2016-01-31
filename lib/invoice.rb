fa8b6c5f048c3cae845f53b0ee63c535dacb1f09
Ben <ben.c.hawker@gmail.com>
2016-01-31 11:56:35 +0800

    Space

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,13 +1,12 @@
 class Invoice
 
-    attr_reader :customer, :products
-    attr_accessor :total
+  attr_reader :customer, :products
+  attr_accessor :total
 
   def initialize(customer, products)
     @products = {}
     @customer = customer
     @payment_recieved = false
   end
-
 
 end
