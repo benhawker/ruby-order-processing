0516b51a6bcf9627c5a932cc0d250403445cb708
Ben <ben.c.hawker@gmail.com>
2016-02-03 20:08:38 +0800

    Invoice spec

--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -1,14 +1,20 @@
 require 'invoice'
+require 'purchase_order'
 
 describe Invoice do
 
   let(:customer) { Customer.new("Bob", :company) }
+  let(:purchase_order) { PurchaseOrder.new(customer) }
   let(:product) { Product.new("guitar", 5) }
-  let(:invoice) { Invoice.new(customer, products) }
+
+  before do
+  	purchase_order.add_product(product, 1)
+    @invoice = purchase_order.create_invoice
+  end
 
   describe "#show_invoice" do
-    xit 'show the invoice on the cmd line with some basic formatting' do
-      expect(invoice.show_invoice).to include "Item: $5.0 / Quantity: 1\n"
+    it 'show the invoice on the cmd line with some basic formatting' do
+      expect(@invoice.show_invoice).to include "Item: $5.0 / Quantity: 1\n"
     end
   end
 
